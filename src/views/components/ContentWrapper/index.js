d726e49c4a83fdd857379928af88561b62378be7
Gayeon <kgy1559@gmail.com>
2020-05-07 23:11:33 +0900

    Implement basic content layout

--- src/views/components/ContentWrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Navigation from 'views/components/Navigation';
-
-const ContentWrapper = () => {
-  return <Navigation />;
-};
-
-export default ContentWrapper;
--- src/views/pages/BoardPage.js
+++ src/views/pages/BoardPage.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import ContentWrapper from 'views/components/ContentWrapper';
+import LayoutHeader from 'views/components/LayoutHeader';
 
 const BoardPage = () => {
-  return <ContentWrapper></ContentWrapper>;
+  const isEmptyPost = true;
+
+  return (
+    <>
+      <LayoutHeader />
+      <div className="board wrap_content">
+        <div className="content_title">
+          <span className="font_bold" style={{ width: '200px' }}>
+            게시판
+          </span>
+          <span className="font_thin">
+            건의사항, 교육문의 등 자유롭게 질문해 주세요 :-)
+          </span>
+        </div>
+        <div style={{ textAlign: 'right' }}>
+          <button className="btn_add font_thin" type="button">
+            글쓰기
+          </button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>등록일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isEmptyPost ? (
+              <tr>
+                <td colSpan={4}>등록된 게시글이 없습니다</td>
+              </tr>
+            ) : null}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default BoardPage;
